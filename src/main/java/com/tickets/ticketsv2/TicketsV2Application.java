f2fe6329bf0bbebd62a67a47dbc6b5a9785885a5
mc139 <maciejandrzejcieslik@gmail.com>
2022-06-27 17:44:05 +0100

    initial commit

---
+++ src/main/java/com/tickets/ticketsv2/TicketsV2Application.java
@@ -0,0 +1,13 @@
+package com.tickets.ticketsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tickets/ticketsv2/TicketsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickets.ticketsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
